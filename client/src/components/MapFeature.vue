e95de99f3b7c07ac97233e13623c6fd461a1eae4
konsavv <ksavvidi@hotmail.com>
2024-04-30 11:19:16 +0300

    Create search request

--- client/src/components/MapFeature.vue
+++ client/src/components/MapFeature.vue
@@ -2,10 +2,37 @@
   <div
     class="w-full md:w-auto absolute md:top-[40px] md:left-[60px] z-[2] flex gap-4 px-6 py-8 md:px-0 md:py-0 bg-transparent"
   >
+    <!-- Search -->
+    <div class="relative flex-1 md:min-w-[350px]">
+      <!-- Input -->
+      <input
+        class="pl-9 pr-4 py-3 text-sm focus:outline-none w-full shadow-md"
+        type="text"
+        placeholder="Start your search"
+        v-model="searchQuery"
+      />
+      <!-- Search Icon -->
+      <div class="absolute top-0 left-[8px] h-full flex items-center">
+        <i class="fas fa-search"></i>
+      </div>
+      <!-- Search result -->
+      <div class="absolute mt-2 w-full">
+        <!-- Results -->
+        <div class="h-[200px] overflow-scroll bg-white rounded-md">
+          <div
+            class="px-4 py-2 flex gap-x-2 cursor-pointer hover:bg-slate-600 hover:text-white"
+          >
+            <i class="fas fa-map-marker-alt"></i>
+            <p class="text-xs">Testing Results</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!-- Geolocation -->
     <div
-      class="px-4 bg-white flex items-center shadow-md rounded-md min-h-[45px]"
-      :class="{ 'bg-slate-600': coords }"
+      class="px-4 flex items-center shadow-md rounded-md min-h-[45px]"
+      :class="{ 'bg-slate-600': coords, 'bg-white': !coords }"
       @click="$emit('getGeolocation')"
     >
       <i
@@ -17,7 +44,33 @@
 </template>
 
 <script>
+import { ref } from "vue";
 export default {
   props: ["coords", "fetchCoords"],
+  setup(props) {
+    //search value that user is typing
+    const searchQuery = ref(null);
+    //data value from API
+    const searchData = ref(null);
+    //set timeout
+    const queryTimeout = ref(null);
+
+    const search = () => {
+      queryTimeout.value = setTimeout(() => {
+        if (searchQuery.value !== "") {
+          const params = new URLSearchParams({
+            fuzzyMatch: true,
+            language: "en",
+            limit: 10,
+            proximity: props.coords
+              ? `${props.coords.lng}, ${props.coords.lat}`
+              : "0,0",
+          });
+        }
+      }, 750);
+    };
+
+    return { searchData, searchQuery, queryTimeout };
+  },
 };
 </script>
